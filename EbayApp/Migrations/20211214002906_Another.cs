50a5dc05a2feb61acda0de28f8b0c731d93b836d
David Sargent <dsargent9182@gmail.com>
2021-12-13 20:12:53 -0500

    New migrations

---
+++ EbayApp/Migrations/20211214002906_Another.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EbayApp.Migrations
+{
+    public partial class Another : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Created",
+                table: "Enrollment",
+                type: "datetime",
+                nullable: false,
+                defaultValueSql: "getdate()");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Created",
+                table: "Course",
+                type: "datetime",
+                nullable: false,
+                defaultValueSql: "getdate()");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Created",
+                table: "Enrollment");
+
+            migrationBuilder.DropColumn(
+                name: "Created",
+                table: "Course");
+        }
+    }
+}
